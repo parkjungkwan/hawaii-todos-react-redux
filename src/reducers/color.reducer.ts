4c7551ec335ec01b2bc053a75dec4d4ba0db7a44
parksrazor <pakjkwan@gmail.com>
2020-07-14 13:08:42 +0900

    13:08

---
+++ src/reducers/color.reducer.ts
@@ -0,0 +1,9 @@
+let color = {}
+let initialState = color ? {color} : {}
+
+export function colorReducers(state = initialState, action) {
+    switch (action.type) {
+        default :
+            return state
+    }
+}
