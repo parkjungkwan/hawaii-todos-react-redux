23fd3595f138a79bb6c9372ed69968e5478fa482
parksrazor <pakjkwan@gmail.com>
2020-07-15 11:52:25 +0900

    11:52

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,24 +1,23 @@
 import {userConstants} from '../constants'
 import {userService} from "../apis";
+import {alertActions} from "./alert.actions";
 
 const userActions = {
     login
 }
+function join(userid, password, name) {
+    alert(`ID: ${userid}, PW: ${password}, Name: ${name}`)
+}
 
-function request(userid:string):object {
-    return { type: userConstants.LOGIN_REQUEST, userid}
-}
-function success(userid:string):object {
-    return { type: userConstants.LOGIN_SUCCESS, userid}
-}
-function failure(userid:string, error:string):object {
-    return { type: userConstants.LOGIN_FAIL, userid, error}
-}
 function login(userid, password) {
     alert(`ID: ${userid}, PW: ${password}`)
 
     return dispatch =>{
         dispatch(request(userid))
     }
+
+    const request = user => { return { type: userConstants.LOGIN_REQUEST, user}}
+    const success = user => { return { type: userConstants.LOGIN_SUCCESS, user}}
+    const failure = user => { return { type: userConstants.LOGIN_FAILURE, user}}
 }
 export default userActions
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render }  from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import store from './helpers/store'
 import * as serviceWorker from './serviceWorker';
 
